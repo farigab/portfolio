35f681a84d56eee1c2135c038e1838b762c3166c
Gabriel F <f-gab@outlook.com>
2026-01-23 08:58:14 -0300

    FEAT: Adição botão para links uteis

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, ButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './about.html',
   styleUrls: ['./about.scss']
 })
-export class AboutComponent { }
+export class AboutComponent {
+  githubUrl = signal('https://github.com/farigab');
+  linkedinUrl = signal('https://www.linkedin.com/in/gabriel-b-f/?locale=en_US');
+}
